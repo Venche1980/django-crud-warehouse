e60ff77c7986247a98feb65b2f0f5e1eb5db2117
Mihail Tarasov <tarmi@rambler.ru>
2025-06-29 18:17:50 +0300

    3

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stocks_db',
+        'USER': 'postgress',
+        'PASSWORD': '123',
+        'HOST': 'db',  # имя сервиса из docker-compose
+        'PORT': '5432'
     }
 }
 
